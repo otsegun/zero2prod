a35ad1cc3553f1b47da5ea6488e38f479cfc102b
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-01 12:36:49 +0200

    tests: implement first integration test

---
+++ src/lib.rs
@@ -0,0 +1,15 @@
+use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
+
+async fn health_check() -> impl Responder {
+    HttpResponse::Ok()
+}
+
+// We need to mark `run` as public.
+// It is no longer a binary entrypoint, therefore we can mark it as async
+// without having to use any proc-macro incantation.
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
+    Ok(server)
+}

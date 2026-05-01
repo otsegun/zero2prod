d957450707a35d138463fc812c25a8a8658cea5a
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-01 14:18:55 +0200

    feature; add form extractor

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,30 @@
+use actix_web::{App, HttpResponse, HttpServer, dev::Server, web};
 use std::net::TcpListener;
 
-use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
 
-async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+async fn health_check() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
 }
 
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     Ok(server)
 }

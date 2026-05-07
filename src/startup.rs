427693c470dba31179cc2dc04c41fa199288771e
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-07 21:28:33 +0200

    actix web workers

--- src/startup.rs
+++ src/startup.rs
@@ -1,4 +1,5 @@
 use actix_web::{App, HttpServer, dev::Server, web};
+use sqlx::PgConnection;
 use std::net::TcpListener;
 
 use crate::routes::{health_check, subscribe};
@@ -6,11 +7,15 @@ use crate::routes::{health_check, subscribe};
 // We need to mark `run` as public.
 // It is no longer a binary entrypoint, therefore we can mark it as async
 // without having to use any proc-macro incantation.
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .app_data(connection.clone())
     })
     .listen(listener)?
     .run();

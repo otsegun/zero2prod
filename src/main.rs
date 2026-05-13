92271c8825a617903bd9fdd7c3f8b5abd5af3f4e
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-13 11:32:17 +0200

    The tracing crate

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use zero2prod::{configuration::get_configuration, startup::run};
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // set logger
-    env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())

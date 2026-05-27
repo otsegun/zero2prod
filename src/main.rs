8ba0bcc7acc870789445ba8f6304d694a571af06
Oluwasegun Ojo <seguntaiwoojo@gmail.com>
2026-05-27 08:09:46 +0200

    Cleaning up initialization

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
-use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{EnvFilter, Registry, layer::SubscriberExt};
-use zero2prod::{configuration::get_configuration, startup::run};
+
+use zero2prod::{
+    configuration::get_configuration, startup::run, telemetry::get_subscriber,
+    telemetry::init_subscriber,
+};
 
 #[tokio::main]
+
 async fn main() -> Result<(), std::io::Error> {
-    // Set logger
-    // env_logger::Builder::from_env(Env::default().default_filter_or("trace")).init();
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new("zero2prod".into(), std::io::stdout);
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
